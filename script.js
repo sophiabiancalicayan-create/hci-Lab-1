e6dea9aa233e8afffc22b4dd038aeb82c9bda89b
sophiabiancalicayan-create <sophiabianca.licayan@hcdc.edu.ph>
2026-03-25 19:13:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirect() {
+    window.location.href = "https://www.google.com";
+  }
+  
+  function showAlert() {
+    alert("Hello!");
+  }
+  
+  function newAlert() {
+    alert(" Click me");
+  }
